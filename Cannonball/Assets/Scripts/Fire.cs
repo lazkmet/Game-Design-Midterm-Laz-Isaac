4d2a4629e20d9d4413e6a833994e2d4256d346fe
lazkmet <lazkmet@gmail.com>
2021-10-18 18:20:18 -0400

    Basic Cannon Firing Controls Implemented

--- Cannonball/Assets/Scripts/Fire.cs
+++ Cannonball/Assets/Scripts/Fire.cs
@@ -6,8 +6,12 @@ public class Fire : MonoBehaviour
 {
     public int ammoRemaining = 0;
     public int maxAmmo = 4;
+    public int maxPower = 1000;
+    public float powerGain = 40;
     public GameObject firingOrigin;
     public GameObject cannonball;
+    private int framesHeld = 0;
+    private float powerPercent = 5.0f;
     void Start()
     {
         ammoRemaining = 4;
@@ -16,6 +20,28 @@ public class Fire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            framesHeld = 0;
+        }
+        else if (Input.GetButton("Jump"))
+        {
+            if (framesHeld < 40)
+            {
+                framesHeld += 1;
+            }
+            else if (framesHeld == 40)
+            {
+                powerPercent = 5.0f;
+                framesHeld += 1;
+            }
+            else if (powerPercent < 100){
+                powerPercent += powerGain * Time.deltaTime;
+            }
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            print("Fire at Power Level: " + powerPercent + "%");
+        }
     }
 }
