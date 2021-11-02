e595333f73736447b86ba83324b1b0bddffabbd2
lazkmet <lazkmet@gmail.com>
2021-11-02 10:54:13 -0400

    Gameplay Workflow Complete

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -36,5 +37,8 @@ public class PauseMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+    public void Restart() {
+        SceneManager.LoadScene("CannonRoom");
     }
 }
